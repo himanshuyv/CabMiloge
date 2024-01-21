c8530db247a8bbe2b8b1575a4c3e23e347de25f4
StarryBadger <shaunak.biswas@research.iiit.ac.in>
2024-01-21 06:46:20 +0530

    password encrypt code

---
+++ encrypt.py
@@ -0,0 +1,10 @@
+from bcrypt import gensalt,hashpw,checkpw
+
+def hash_password(password):
+    salt = gensalt()
+    hashed_password = hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
+def check_password(entered_password, hashed_password):
+    return checkpw(entered_password.encode('utf-8'), hashed_password)
+
