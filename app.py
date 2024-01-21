75352cfa5593fbb39a320e60c4a58ce1165e4035
Jayesh <jayeshsutar123456@gmail.com>
2024-01-21 05:58:22 +0530

    modified signup

--- app.py
+++ app.py
@@ -8,28 +8,71 @@ def LogIn():
     return render_template('LogIn.html')
 
 
-@app.route('/index')
-def index():
-    # conn = sqlite3.connect('cabmates.db') 
-    # cursor = conn.cursor()
-    # try:
-    #     cursor.execute( '''
-    #                     SELECT * FROM Cabmate
-    #                     ''')
-    #     entry=cursor.fetchall()
-    #     conn.commit()
-    #     conn.close()
+@app.route('/Get_Auth')
+def Get_Auth():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
        
-    #     return render_template('test.html')
-    # except:
-    #     print('login failed')
-    #     return render_template('testlogin.html')
-    # return render_template('index.html',)
-    return render_template('index.html')
+
+        conn = sqlite3.connect('cabmates.db') 
+        cursor = conn.cursor()
+        try:
+            cursor.execute( '''
+                            SELECT * FROM Login WHERE Email = ?
+                            ''', (email,))
+            entry=cursor.fetchone()
+            conn.commit()
+            conn.close()
+            if entry[3] == password :
+                print('login sucessfull')
+                return render_template('index.html')
+            else :
+                message='wrong password!'
+                return render_template('LogIn.html')
+        except:
+            print('login failed')
+            message='Email not found!'
+            return render_template('LogIn.html')
+        
+
+
+@app.route('/Get_userData')
+def Get_userData():
+    if request.method == 'POST':
+        fname = request.form['fname']
+        lname = request.form['lname']
+        email = request.form['email']
+        password = request.form['password']
+
+       
+
+        conn = sqlite3.connect('cabmates.db') 
+        cursor = conn.cursor()
+        try:
+            cursor.execute( '''
+                            SELECT * FROM Login WHERE Email = ?
+                            ''', (email,))
+            entry=cursor.fetchone()
+            conn.commit()
+            conn.close()
+            if entry[3] == password :
+                print('login sucessfull')
+                return render_template('index.html')
+            else :
+                message='wrong password!'
+                return render_template('LogIn.html')
+        except:
+            print('login failed')
+            message='Email not found!'
+            return render_template('SignUp.html',message=message)
 
 @app.route('/SignUp')
 def SignUp():
     return render_template('SignUp.html')
+
+
+
 
 
 
